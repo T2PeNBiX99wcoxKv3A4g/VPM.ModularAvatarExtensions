4a6a9c0758ca033ef6388f39d6a63aefeaa8ff2c
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-11-14 13:40:18 +0800

    feat(TurnOffInBuildGenerator): Add animator controller generation for TurnOffInBuild feature

--- Packages/io.github.ykysnk.modular-avatar-extensions/Editor/TurnOffInBuildGenerator.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Editor/TurnOffInBuildGenerator.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using AnimatorAsCode.V1;
 using io.github.ykysnk.ModularAvatarExtensions.Editor;
 using io.github.ykysnk.utils.Extensions;
+using nadena.dev.modular_avatar.core;
 using nadena.dev.ndmf;
+using UnityEditor;
 using UnityEngine;
+using VRC.SDK3.Avatars.Components;
 
 [assembly: ExportsPlugin(typeof(TurnOffInBuildGenerator))]
 
@@ -11,6 +16,10 @@ public class TurnOffInBuildGenerator : Plugin<TurnOffInBuildGenerator>, IMaexPlu
 {
     public override string QualifiedName => "io.github.ykysnk.ModularAvatarExtensions.TurnOffInBuild";
     public override string DisplayName => "Modular Avatar Extensions Turn Off In Build Generator";
+    private const string SystemName = "Modular Avatar EX";
+    private const string GenName = "Turn Off In Build";
+    private int _mergeAnimatorIndex;
+    private GameObject? _root;
 
     public void Log(object message) => Debug.Log($"[{DisplayName}] {message}");
 
@@ -33,14 +42,74 @@ public class TurnOffInBuildGenerator : Plugin<TurnOffInBuildGenerator>, IMaexPlu
     {
         var avatar = ctx.AvatarRootObject;
         var turnOffInBuilds = avatar.GetComponentsInChildren<TurnOffInBuild>(true);
+        var animObjs = new List<GameObject>();
 
         Log($"Find {turnOffInBuilds.Length} turn off in build inside \"{avatar.FullName()}\"");
 
         foreach (var turnOffInBuild in turnOffInBuilds)
         {
             var obj = turnOffInBuild.gameObject;
+            if (!obj.activeSelf)
+            {
+                Log($"Game Object \"{obj.FullName()}\" already is non active");
+                continue;
+            }
+
             obj.SetActive(false);
+            animObjs.Add(obj);
             Log($"Game Object \"{obj.FullName()}\" is now non active");
         }
+
+        var aac = AacV1.Create(new()
+        {
+            SystemName = SystemName,
+            AnimatorRoot = ctx.AvatarRootTransform,
+            DefaultValueRoot = ctx.AvatarRootTransform,
+            AssetKey = GUID.Generate().ToString(),
+            AssetContainer = ctx.AssetContainer,
+            ContainerMode = AacConfiguration.Container.OnlyWhenPersistenceRequired,
+            DefaultsProvider = new AacDefaultsProvider(true),
+        });
+
+        var ctrl = aac.NewAnimatorController();
+        var layer = ctrl.NewLayer(GenName);
+
+        var clip = aac.NewClip();
+        clip.Toggling(animObjs.ToArray(), false);
+
+        layer.NewState("Turn Off").WithAnimation(clip);
+
+        var newObj = new GameObject($"{SystemName}: {GenName}")
+        {
+            transform =
+            {
+                parent = ctx.AvatarRootTransform
+            }
+        };
+        _root = newObj;
+        var mergeAnimator = NewMergeAnimator(ctrl, VRCAvatarDescriptor.AnimLayerType.FX);
+        if (!mergeAnimator) return;
+        mergeAnimator!.layerPriority = -999999;
+        mergeAnimator.deleteAttachedAnimator = true;
+        mergeAnimator.matchAvatarWriteDefaults = true;
+    }
+
+    // Copy from MaAc
+    private ModularAvatarMergeAnimator? NewMergeAnimator(AacFlController controller,
+        VRCAvatarDescriptor.AnimLayerType layerType)
+    {
+        if (!_root) return null;
+
+        var components = _root!.GetComponents<ModularAvatarMergeAnimator>();
+        var mergeAnimator = _mergeAnimatorIndex >= components.Length
+            ? Undo.AddComponent<ModularAvatarMergeAnimator>(_root)
+            : components[_mergeAnimatorIndex];
+
+        _mergeAnimatorIndex++;
+
+        mergeAnimator.animator = controller.AnimatorController;
+        mergeAnimator.layerType = layerType;
+        mergeAnimator.pathMode = MergeAnimatorPathMode.Absolute;
+        return mergeAnimator;
     }
 }
