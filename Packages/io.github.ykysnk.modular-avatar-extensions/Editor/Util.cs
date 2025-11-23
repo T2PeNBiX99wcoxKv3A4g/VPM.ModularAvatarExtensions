9c1ab6b4cbc716231946abbd302f86d092368186
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-11-23 14:47:31 +0800

    fix: Implement icon assignment for `AvatarMaexComponent` scripts and improve `DisableMaexGizmoIcons` logic

--- Packages/io.github.ykysnk.modular-avatar-extensions/Editor/Util.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Editor/Util.cs
@@ -1,6 +1,8 @@
 using System;
+using System.IO;
 using System.Linq;
 using HarmonyLib;
+using io.github.ykysnk.utils;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,13 +11,17 @@ namespace io.github.ykysnk.ModularAvatarExtensions.Editor;
 [InitializeOnLoad]
 internal static class Util
 {
-    static Util() => EditorApplication.update += TryDisableMaexGizmoIcons;
+    private const string IconPath =
+        "Packages/io.github.ykysnk.modular-avatar-extensions/Runtime/Icons/Icon_MAEX_Script.png";
 
-    private static void TryDisableMaexGizmoIcons()
+    static Util() => EditorApplication.update += TryRun;
+
+    private static void TryRun()
     {
         try
         {
             DisableMaexGizmoIcons();
+            ChangeMaexIcons();
         }
         catch (Exception e)
         {
@@ -23,7 +29,7 @@ internal static class Util
         }
         finally
         {
-            EditorApplication.update -= TryDisableMaexGizmoIcons;
+            EditorApplication.update -= TryRun;
         }
     }
 
@@ -52,7 +58,33 @@ internal static class Util
             if (typeof(AvatarMaexComponent).IsAssignableFrom(ty) && !ty.IsAbstract)
                 GizmoUtility.SetIconEnabled(ty, false);
 
-        Debug.Log("Disable MAEX gizmo icons");
+        Utils.Log(nameof(DisableMaexGizmoIcons), "Disable MAEX gizmo icons");
         SessionState.SetString("MAEXIconsDisabled", dateTime);
+    }
+
+    private static void ChangeMaexIcons()
+    {
+        Texture2D? maexIcon = null;
+
+        if (File.Exists(IconPath))
+            maexIcon = AssetDatabase.LoadAssetAtPath<Texture2D>(IconPath);
+
+        foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+        foreach (var ty in assembly.GetTypes())
+            if (typeof(AvatarMaexComponent).IsAssignableFrom(ty) && !ty.IsAbstract)
+            {
+                var path = $"Packages/io.github.ykysnk.modular-avatar-extensions/Runtime/{ty.Name}.cs";
+
+                Utils.Log(nameof(ChangeMaexIcons), $"Test: Path {path}");
+
+                if (!File.Exists(path) || AssetImporter.GetAtPath(path) is not MonoImporter monoImporter ||
+                    maexIcon == null ||
+                    monoImporter.GetIcon() == maexIcon)
+                    continue;
+
+                monoImporter.SetIcon(maexIcon);
+                monoImporter.SaveAndReimport();
+                Utils.Log(nameof(ChangeMaexIcons), $"Change to MAEX icon: {path}");
+            }
     }
 }
