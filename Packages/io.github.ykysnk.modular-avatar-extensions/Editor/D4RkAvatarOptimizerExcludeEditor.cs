81809b69e070e80f94fed39b4362f6a70828ad31
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-04 20:40:49 +0800

    feat: Add support for `d4rkAvatarOptimizer` with new runtime and editor components
    Introduce compatibility with `d4rkAvatarOptimizer` by adding conditional compilation defines and dependency updates. Add `ModularAvatarExtensionsD4RkAvatarOptimizerExclude` runtime component and its custom editor with localization support. Update assembly definitions and `vpm-manifest.json` to include `d4rkAvatarOptimizer`.

---
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Editor/D4RkAvatarOptimizerExcludeEditor.cs
@@ -0,0 +1,15 @@
+#if D4RK_AVATAR_OPTIMIZER
+using UnityEditor;
+
+namespace io.github.ykysnk.ModularAvatarExtensions.Editor;
+
+[CustomEditor(typeof(ModularAvatarExtensionsD4RkAvatarOptimizerExclude))]
+[CanEditMultipleObjects]
+public class D4RkAvatarOptimizerExcludeEditor : MaexEditor
+{
+    protected override void OnInnerInspectorGUI()
+    {
+        EditorGUILayout.HelpBox("label.d4rk_avatar_optimizer_exclude.info".S(), MessageType.Info, true);
+    }
+}
+#endif
---
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Runtime/ModularAvatarExtensionsD4RkAvatarOptimizerExclude.cs
@@ -0,0 +1,13 @@
+#if D4RK_AVATAR_OPTIMIZER
+using UnityEngine;
+
+namespace io.github.ykysnk.ModularAvatarExtensions
+{
+    [DisallowMultipleComponent]
+    [AddComponentMenu("Modular Avatar EX/MAEX d4rk Avatar Optimizer Exclude")]
+    public class ModularAvatarExtensionsD4RkAvatarOptimizerExclude : AvatarMaexComponent
+    {
+        public override bool DontDestroyOnBuild => true;
+    }
+}
+#endif
