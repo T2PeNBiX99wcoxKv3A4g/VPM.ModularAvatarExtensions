f545ced66522659715d563c5720c0a7b5b07df15
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-11 06:32:01 +0800

    refactor: Normalize namespace brace style and formatting in D4RkAvatarOptimizer-related scripts

--- Packages/io.github.ykysnk.modular-avatar-extensions/Editor/D4RkAvatarOptimizerExcludeEditor.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Editor/D4RkAvatarOptimizerExcludeEditor.cs
@@ -1,15 +1,16 @@
 #if MAEX_D4RK_AVATAR_OPTIMIZER
 using UnityEditor;
 
-namespace io.github.ykysnk.ModularAvatarExtensions.Editor;
-
-[CustomEditor(typeof(ModularAvatarExtensionsD4RkAvatarOptimizerExclude))]
-[CanEditMultipleObjects]
-internal class D4RkAvatarOptimizerExcludeEditor : MaexEditor
+namespace io.github.ykysnk.ModularAvatarExtensions.Editor
 {
-    protected override void OnInnerInspectorGUI()
+    [CustomEditor(typeof(ModularAvatarExtensionsD4RkAvatarOptimizerExclude))]
+    [CanEditMultipleObjects]
+    internal class D4RkAvatarOptimizerExcludeEditor : MaexEditor
     {
-        EditorGUILayout.HelpBox("label.d4rk_avatar_optimizer_exclude.info".S(), MessageType.Info, true);
+        protected override void OnInnerInspectorGUI()
+        {
+            EditorGUILayout.HelpBox("label.d4rk_avatar_optimizer_exclude.info".S(), MessageType.Info, true);
+        }
     }
-}
 #endif
+}
--- Packages/io.github.ykysnk.modular-avatar-extensions/Editor/D4RkAvatarOptimizerPatches.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Editor/D4RkAvatarOptimizerPatches.cs
@@ -9,56 +9,57 @@ using io.github.ykysnk.utils;
 using UnityEditor;
 using UnityEngine;
 
-namespace io.github.ykysnk.ModularAvatarExtensions.Editor;
-
-internal static class D4RkAvatarOptimizerPatches
+namespace io.github.ykysnk.ModularAvatarExtensions.Editor
 {
-    private const string PatchId = "io.github.ykysnk.modular-avatar-extensions.d4rk-avatar-optimizer-patches";
-    private const string CacheGetAllExcludedTransformsName = "cache_GetAllExcludedTransforms";
-
-    [InitializeOnLoadMethod]
-    [MenuItem("Tools/Modular Avatar EX/ReInitialize D4Rk Avatar Optimizer Patches")]
-    private static void Initialize()
+    internal static class D4RkAvatarOptimizerPatches
     {
-        var harmony = new Harmony(PatchId);
-        var targetMethod = AccessTools.Method(typeof(d4rkAvatarOptimizer),
-            nameof(d4rkAvatarOptimizer.GetAllExcludedTransforms));
-        harmony.CreateReversePatcher(targetMethod,
-            new(typeof(D4RkAvatarOptimizerPatches), nameof(OrigGetAllExcludedTransforms))).Patch();
-        harmony.Patch(targetMethod, new(typeof(D4RkAvatarOptimizerPatches), nameof(GetAllExcludedTransforms)));
-        AssemblyReloadEvents.beforeAssemblyReload += () => harmony.UnpatchAll(PatchId);
-        Utils.Log(nameof(D4RkAvatarOptimizerPatches), "D4Rk Avatar Optimizer Patches Initialized");
-    }
+        private const string PatchId = "io.github.ykysnk.modular-avatar-extensions.d4rk-avatar-optimizer-patches";
+        private const string CacheGetAllExcludedTransformsName = "cache_GetAllExcludedTransforms";
 
-    [SuppressMessage("ReSharper", "UnusedParameter.Local")]
-    private static HashSet<Transform> OrigGetAllExcludedTransforms(d4rkAvatarOptimizer instance) =>
-        throw new NotImplementedException("HarmonyReversePatch is fucked");
-
-    [SuppressMessage("ReSharper", "RedundantAssignment")]
-    private static bool GetAllExcludedTransforms(d4rkAvatarOptimizer __instance, ref HashSet<Transform> __result)
-    {
-        var cacheGetAllExcludedTransforms = Traverse.Create(__instance)
-            .Field<HashSet<Transform>>(CacheGetAllExcludedTransformsName).Value;
-        if (cacheGetAllExcludedTransforms != null)
+        [InitializeOnLoadMethod]
+        [MenuItem("Tools/Modular Avatar EX/ReInitialize D4Rk Avatar Optimizer Patches")]
+        private static void Initialize()
         {
-            __result = cacheGetAllExcludedTransforms;
+            var harmony = new Harmony(PatchId);
+            var targetMethod = AccessTools.Method(typeof(d4rkAvatarOptimizer),
+                nameof(d4rkAvatarOptimizer.GetAllExcludedTransforms));
+            harmony.CreateReversePatcher(targetMethod,
+                new(typeof(D4RkAvatarOptimizerPatches), nameof(OrigGetAllExcludedTransforms))).Patch();
+            harmony.Patch(targetMethod, new(typeof(D4RkAvatarOptimizerPatches), nameof(GetAllExcludedTransforms)));
+            AssemblyReloadEvents.beforeAssemblyReload += () => harmony.UnpatchAll(PatchId);
+            Utils.Log(nameof(D4RkAvatarOptimizerPatches), "D4Rk Avatar Optimizer Patches Initialized");
+        }
+
+        [SuppressMessage("ReSharper", "UnusedParameter.Local")]
+        private static HashSet<Transform> OrigGetAllExcludedTransforms(d4rkAvatarOptimizer instance) =>
+            throw new NotImplementedException("HarmonyReversePatch is fucked");
+
+        [SuppressMessage("ReSharper", "RedundantAssignment")]
+        private static bool GetAllExcludedTransforms(d4rkAvatarOptimizer __instance, ref HashSet<Transform> __result)
+        {
+            var cacheGetAllExcludedTransforms = Traverse.Create(__instance)
+                .Field<HashSet<Transform>>(CacheGetAllExcludedTransformsName).Value;
+            if (cacheGetAllExcludedTransforms != null)
+            {
+                __result = cacheGetAllExcludedTransforms;
+                return false;
+            }
+
+            var origList = OrigGetAllExcludedTransforms(__instance);
+            var exExclusions = new List<Transform>();
+            exExclusions.AddRange(__instance.transform
+                .GetComponentsInChildren<ModularAvatarExtensionsD4RkAvatarOptimizerExclude>(true)
+                .Select(c => c.transform));
+            foreach (var excludedTransform in exExclusions.Where(excludedTransform => excludedTransform != null))
+            {
+                origList.Add(excludedTransform);
+                origList.UnionWith(excludedTransform.GetAllDescendants());
+            }
+
+            Utils.Log(nameof(D4RkAvatarOptimizerPatches), $"Cached {origList.Count} transforms");
+            __result = origList;
             return false;
         }
-
-        var origList = OrigGetAllExcludedTransforms(__instance);
-        var exExclusions = new List<Transform>();
-        exExclusions.AddRange(__instance.transform
-            .GetComponentsInChildren<ModularAvatarExtensionsD4RkAvatarOptimizerExclude>(true)
-            .Select(c => c.transform));
-        foreach (var excludedTransform in exExclusions.Where(excludedTransform => excludedTransform != null))
-        {
-            origList.Add(excludedTransform);
-            origList.UnionWith(excludedTransform.GetAllDescendants());
-        }
-
-        Utils.Log(nameof(D4RkAvatarOptimizerPatches), $"Cached {origList.Count} transforms");
-        __result = origList;
-        return false;
     }
-}
 #endif
+}
