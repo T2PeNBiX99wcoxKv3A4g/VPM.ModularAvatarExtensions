0fc1e9bb28fef44be2304700070f36f9d300938a
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-12 07:01:20 +0800

    fix: Expand renderer and mesh retrieval to include child objects

--- Packages/io.github.ykysnk.modular-avatar-extensions/Runtime/MeshData.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Runtime/MeshData.cs
@@ -14,22 +14,40 @@ namespace io.github.ykysnk.ModularAvatarExtensions
             GameObject = gameObject;
             Mesh = GetMesh(gameObject);
             Materials = GetMaterials(gameObject);
-            Renderer = gameObject.TryGetComponent<Renderer>(out var renderer) ? renderer : null;
+            Renderer = TryGetRenderer(gameObject);
+        }
+
+        private static Renderer? TryGetRenderer(GameObject obj)
+        {
+            if (obj.TryGetComponent<Renderer>(out var renderer))
+                return renderer;
+            var renderers = obj.GetComponentsInChildren<Renderer>(true);
+            return renderers.Length > 0 ? renderers[0] : null;
         }
 
         private static Mesh? GetMesh(GameObject obj)
         {
             if (obj.TryGetComponent<SkinnedMeshRenderer>(out var skinnedMeshRenderer))
                 return skinnedMeshRenderer.sharedMesh;
-            return obj.TryGetComponent<MeshFilter>(out var meshFilter) ? meshFilter.sharedMesh : null;
+            if (obj.TryGetComponent<MeshFilter>(out var meshFilter))
+                return meshFilter.sharedMesh;
+            var skinnedMeshRenderers = obj.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+            if (skinnedMeshRenderers.Length > 0) return skinnedMeshRenderers[0].sharedMesh;
+            var meshFilters = obj.GetComponentsInChildren<MeshFilter>(true);
+            return meshFilters.Length > 0 ? meshFilters[0].sharedMesh : null;
         }
 
         private static Material[] GetMaterials(GameObject obj)
         {
             if (obj.TryGetComponent<SkinnedMeshRenderer>(out var skinnedMeshRenderer))
                 return skinnedMeshRenderer.sharedMaterials;
-            return obj.TryGetComponent<MeshRenderer>(out var meshRenderer)
-                ? meshRenderer.materials
+            if (obj.TryGetComponent<MeshRenderer>(out var meshRenderer))
+                return meshRenderer.materials;
+            var skinnedMeshRenderers = obj.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+            if (skinnedMeshRenderers.Length > 0) return skinnedMeshRenderers[0].sharedMaterials;
+            var meshRenderers = obj.GetComponentsInChildren<MeshRenderer>(true);
+            return meshRenderers.Length > 0
+                ? meshRenderers[0].materials
                 : new Material[]
                 {
                 };
