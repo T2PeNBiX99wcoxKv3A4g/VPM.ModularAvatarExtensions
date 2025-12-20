a544092c9f4a6564853d38a81df75979e908a7e6
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-20 13:27:03 +0800

    feat: Implement ChangeMaterialInBuildPass with material assignment logic during build

---
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Editor/ChangeMaterialInBuildPass.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using io.github.ykysnk.utils.Extensions;
+using nadena.dev.ndmf;
+using UnityEngine;
+
+namespace io.github.ykysnk.ModularAvatarExtensions.Editor
+{
+    internal class ChangeMaterialInBuildPass : MaexPass<ChangeMaterialInBuildPass>
+    {
+        public override string QualifiedName => "io.github.ykysnk.ModularAvatarExtensions.ChangeMaterialInBuild";
+        public override string DisplayName => "Modular Avatar Extensions Change Material In Build";
+
+        protected override void Execute(BuildContext ctx)
+        {
+            var avatar = ctx.AvatarRootObject;
+            var changeMaterialInBuilds =
+                avatar.GetComponentsInChildren<ModularAvatarExtensionsChangeMaterialInBuild>(true).Where(c => c)
+                    .ToArray();
+
+            LogC($"Find {changeMaterialInBuilds.Length} change material in build inside \"{avatar.FullName()}\"");
+
+            foreach (var changeMaterialInBuild in changeMaterialInBuilds)
+                using (ErrorReport.WithContextObject(changeMaterialInBuild))
+                    try
+                    {
+                        var renderer = changeMaterialInBuild.GetComponent<Renderer>();
+                        var materialChangeDatas = changeMaterialInBuild.MaterialChangeDatas.Distinct().ToArray();
+
+                        if (renderer == null)
+                            throw new("Renderer is not assigned or found in the target component.");
+
+                        var newMaterials = new Material[renderer.sharedMaterials.Length];
+
+                        foreach (var materialChangeData in materialChangeDatas)
+                            if (!newMaterials.TrySetValue(materialChangeData.materialIndex, materialChangeData.material))
+                                throw new ArgumentOutOfRangeException(nameof(materialChangeData.materialIndex),
+                                    $"Material index {materialChangeData.materialIndex} is out of range.");
+
+                        renderer.materials = newMaterials;
+                    }
+                    catch (Exception e)
+                    {
+                        ErrorReport.ReportException(e);
+                        return;
+                    }
+        }
+    }
+}
