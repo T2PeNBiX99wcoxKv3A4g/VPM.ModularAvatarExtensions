62f6c360f123db03ed457d7c2207664689523b8f
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-24 06:30:18 +0800

    feat: Add dropdown for adding extra reference data in Icon Generator and enrich UXML layouts with additional fields

--- Packages/io.github.ykysnk.modular-avatar-extensions/Editor/IconGeneratorOfReferenceEditor.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Editor/IconGeneratorOfReferenceEditor.cs
@@ -1,4 +1,7 @@
+using System.Linq;
+using System.Reflection;
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace io.github.ykysnk.ModularAvatarExtensions.Editor
@@ -9,6 +12,32 @@ namespace io.github.ykysnk.ModularAvatarExtensions.Editor
     {
         protected override void OnCreateInnerInspectorGUI(TemplateContainer container)
         {
+            var referenceExtraDatasAdd = container.Q<DropdownField>("referenceExtraDatasAdd");
+            var type = typeof(IReferenceExtraData);
+            var types = type.Assembly.GetTypes().Where(t =>
+                t is { IsClass: true, IsAbstract: false, IsInterface: false } && type.IsAssignableFrom(t));
+            var nameWithTypes = types.ToDictionary(x =>
+            {
+                var attribute = x.GetCustomAttribute<AddComponentMenu>();
+                return attribute == null ? x.FullName : attribute.componentMenu.Split('/').Last();
+            });
+            referenceExtraDatasAdd.choices = nameWithTypes.Keys.ToList();
+            referenceExtraDatasAdd.formatSelectedValueCallback +=
+                _ => "label.icon_generator_of_reference.add_reference_extra_data".S();
+            referenceExtraDatasAdd.RegisterValueChangedCallback(evt =>
+            {
+                if (!nameWithTypes.TryGetValue(evt.newValue, out var addType)) return;
+                foreach (var component in targets)
+                {
+                    if (component is not ModularAvatarExtensionsIconGeneratorOfReference iconGeneratorOfReference)
+                        continue;
+                    var obj = iconGeneratorOfReference.gameObject;
+                    Undo.AddComponent(obj, addType);
+                    EditorUtility.SetDirty(obj);
+                }
+
+                referenceExtraDatasAdd.value = "";
+            });
         }
     }
 }
