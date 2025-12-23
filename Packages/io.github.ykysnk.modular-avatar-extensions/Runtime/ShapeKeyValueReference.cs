aeb05faecb963db99250daba9112f614aec34112
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-24 06:32:17 +0800

    refactor: Remove `IReferenceExtraData` implementation and add `AvatarObjectReference` to `ShapeKeyValueReference`

--- Packages/io.github.ykysnk.modular-avatar-extensions/Runtime/ShapeKeyValueReference.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Runtime/ShapeKeyValueReference.cs
@@ -1,10 +1,12 @@
 using System;
+using nadena.dev.modular_avatar.core;
 
 namespace io.github.ykysnk.ModularAvatarExtensions
 {
     [Serializable]
-    public struct ShapeKeyValueReference : IReferenceExtraData
+    public struct ShapeKeyValueReference
     {
+        public AvatarObjectReference reference;
         public string shapeKeyName;
         public float value;
     }
