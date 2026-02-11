b9c9ad79e1a999cc0a0246e73417a40dd16c65af
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-02-12 02:02:24 +0800

    fix: Prevent `GetOrCreate` from being called at runtime by adding editor-only constraints

--- Packages/io.github.ykysnk.modular-avatar-extensions/Runtime/ModularAvatarExtensionsIcon.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Runtime/ModularAvatarExtensionsIcon.cs
@@ -9,12 +9,17 @@ namespace io.github.ykysnk.ModularAvatarExtensions
 
         public static ModularAvatarExtensionsIcon GetOrCreate(string path)
         {
+            // Why the fuck this object be loaded in runtime?
+#if UNITY_EDITOR
             var asset = AssetDatabase.LoadAssetAtPath<ModularAvatarExtensionsIcon>(path);
             if (asset != null) return asset;
             asset = CreateInstance<ModularAvatarExtensionsIcon>();
             AssetDatabase.CreateAsset(asset, path);
             AssetDatabase.SaveAssets();
             return asset;
+#else
+            throw new("Do not call this method in runtime.");
+#endif
         }
 
         public void Save() => EditorUtility.SetDirty(this);
