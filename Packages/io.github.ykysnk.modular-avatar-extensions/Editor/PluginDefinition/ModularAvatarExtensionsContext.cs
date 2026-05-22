4c124b1ef26fc0420be0164379e29d750d10576c
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-05-23 07:23:39 +0800

    feat: add `AddComponent` method and `Retained` class to manage component paths in build context

--- Packages/io.github.ykysnk.modular-avatar-extensions/Editor/PluginDefinition/ModularAvatarExtensionsContext.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions/Editor/PluginDefinition/ModularAvatarExtensionsContext.cs
@@ -1,25 +1,40 @@
 using System;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using nadena.dev.ndmf;
 
 namespace io.github.ykysnk.ModularAvatarExtensions.Editor.PluginDefinition
 {
     // Copy from nadena.dev.modular_avatar.core.editor.ModularAvatarContext
-    [PublicAPI]
     internal class ModularAvatarExtensionsContext : IExtensionContext
     {
+        private Retained _retained = null!;
         private IDisposable? _toDispose;
         internal MaexBuildContext? BuildContext { get; private set; }
 
         public void OnActivate(BuildContext context)
         {
             BuildContext ??= new(context);
+            _retained = context.GetState<Retained>();
         }
 
         public void OnDeactivate(BuildContext context)
         {
             _toDispose?.Dispose();
             _toDispose = null;
+        }
+
+        [PublicAPI]
+        public void AddComponent(AvatarMaexComponent component, string? path)
+        {
+            if (component == null) return;
+            _retained.ComponentFullPathProps[component] = path ?? component.name;
+        }
+
+        [PublicAPI]
+        public class Retained
+        {
+            public readonly Dictionary<AvatarMaexComponent, string> ComponentFullPathProps = new();
         }
     }
 }
